47cec3af7625bc4b1392d67045d459db8051432a
jaatsiyah <jaatsiyah@student.telkomuniversity.ac.id>
2023-03-28 15:58:18 +0700

    first commit

---
+++ SayaTubeUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class SayaTubeUser
+{
+	private int id;
+	private List<SayaTubeUser> uploadedVideos;
+	public string username;
+	public SayaTubeUser()
+	{
+	}
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class SayaTubeVideo
+{
+	private int id;
+	private string title;
+	private int playCount;
+	public SayaTubeVideo(string title)
+	{
+		Random random= new Random();
+		this.title = title;
+		id = random.Next(10000, 99999);
+		playCount= 0;
+	}
+
+	public void IncreasePlayCount(int playCount)
+	{
+		this.playCount = playCount;
+	}
+
+	public void PrintVideoDetails()
+	{
+		Console.WriteLine("Details Video : ");
+		Console.WriteLine("ID : ", id);
+		Console.WriteLine("Title : ", title);
+		Console.WriteLine("Play Count : ", playCount);
+
+    }
+}
